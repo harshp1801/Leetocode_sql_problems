048d10258bcd7b1ffedd780f221a64fe757347ce
Harsh Pandey <128036066+harshp1801@users.noreply.github.com>
2025-09-01 22:33:52 +0530

    Time: 469 ms (32.79%), Space: 0B (100%) - LeetHub

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -0,0 +1,8 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+    select max(salary) from 
+    (select salary,dense_rank() over (order by salary desc) rnk from Employee) as a
+    where rnk = N 
+  );
+END
